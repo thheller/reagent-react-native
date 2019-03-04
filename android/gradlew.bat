ecfbfdfeb14a07cdda63d61839bbd3d39e6f75f4
Thomas Heller <th.heller@gmail.com>
2019-03-04 13:19:08 +0100

    remove gradlew.bat hardcoded path

--- android/gradlew.bat
+++ android/gradlew.bat
@@ -17,8 +17,9 @@ set APP_HOME=%DIRNAME%
 set DEFAULT_JVM_OPTS=
 
 @rem Find java.exe
+if defined JAVA_HOME goto findJavaFromJavaHome
 
-set JAVA_EXE=C:\Users\thheller\install\jdk8u192-b12\bin\java.exe
+set JAVA_EXE=java.exe
 %JAVA_EXE% -version >NUL 2>&1
 if "%ERRORLEVEL%" == "0" goto init
 
